df70bd539bcc4e599074ffb7d63f6ea699341fd1
kimtaesu <kimtaesoo188@gmail.com>
2019-10-20 00:38:51 +0900

    ADT 코드 추가.

--- Swift/Programmer/Programmer/Example/Paper.swift
+++ Swift/Programmer/Programmer/Example/Paper.swift
@@ -8,9 +8,36 @@
 
 import Foundation
 
-protocol Paper {
-//    associatedtype T: Programmer
-//    func setData(programmer: T)
+class Paper {
+    private let isClient: Bool
+
+    public init(isClient: Bool) {
+        self.isClient = isClient
+    }
     
-    func run() -> [Program]
+    let library = Library(name: "vueJS")
+    let language = Language(name: "kotlinJS")
+    var programmer: HasProgrammer?
+    let server = Server(name: "test")
+    let backEndLanguage = Language(name: "java")
+    let frontEndLanguage = Language(name: "kotlinJS")
+    
+    private var backEndProgrammer: HasProgrammer?
+    private var frontEndProgrammer: HasProgrammer?
+    
+    func setBackEndProgrammer(_ p: HasProgrammer) {
+        if isClient {
+            self.backEndProgrammer = p
+        }
+    }
+    func setFrontEndProgrammer(_ p: HasProgrammer) {
+        if isClient {
+            self.frontEndProgrammer = p
+        }
+    }
+    func setProgrammer(_ p: HasProgrammer) {
+        if isClient {
+            self.programmer = p
+        }
+    }
 }
